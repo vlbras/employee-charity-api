afec7de28c293d09cb456153bc7038a9cade4688
vlbras <vlad.bras02@gmail.com>
2023-10-21 01:22:12 +0200

    feat: use redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { RewardsModule } from './rewards/rewards.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -16,6 +18,13 @@ import { RewardsModule } from './rewards/rewards.module';
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     RewardsModule,
+    CacheModule.register({
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: +process.env.REDIS_PORT,
+      ttl: parseInt(process.env.REDIS_TTL),
+      isGlobal: true,
+    }),
   ],
 })
 export class AppModule {}
--- src/rewards/rewards.resolver.ts
+++ src/rewards/rewards.resolver.ts
@@ -1,11 +1,15 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { RewardsService } from './rewards.service';
 import { Reward } from '../rewards/models/reward.model';
+import { CacheInterceptor, CacheKey } from '@nestjs/cache-manager';
+import { UseInterceptors } from '@nestjs/common';
 
 @Resolver()
 export class RewardsResolver {
   constructor(private readonly rewardService: RewardsService) {}
 
+  @CacheKey('rewards')
+  @UseInterceptors(CacheInterceptor)
   @Query(() => [Reward], { name: 'rewards' })
   findAll(): Promise<Reward[]> {
     return this.rewardService.findAll();
